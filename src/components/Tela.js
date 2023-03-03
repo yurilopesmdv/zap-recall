a1956d036d78e73ba77ab882b4dce60a5d053b55
yurilopesm <yurilopesm@hotmail.com>
2023-03-03 15:51:25 -0300

    Arquivos Criados

---
+++ src/components/Tela.js
@@ -0,0 +1,12 @@
+import Cards from "./Cards";
+import Footer from "./Footer";
+import Logo from "./Logo";
+export default function Tela(){
+    return (
+        <div>
+            <Logo />
+            <Cards />
+            <Footer />
+        </div>
+    )
+}
